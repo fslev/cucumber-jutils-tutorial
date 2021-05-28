951a4bedb95e0ca0e54376e7763f9206b2225ba9
Florin Slevoaca <florin.slevoaca@ionos.com>
2021-05-28 12:24:53 +0300

    refactoring

--- src/main/java/com/cucumber/tutorial/context/services/http/RestService.java
+++ src/main/java/com/cucumber/tutorial/context/services/http/RestService.java
@@ -2,18 +2,16 @@ package com.cucumber.tutorial.context.services.http;
 
 import com.cucumber.tutorial.context.BaseScenario;
 import io.jtest.utils.clients.http.HttpClient;
+import io.jtest.utils.clients.http.wrappers.HttpResponseWrapper;
 import io.jtest.utils.common.JsonUtils;
 import io.jtest.utils.matcher.ObjectMatcher;
 import io.jtest.utils.matcher.condition.MatchCondition;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Map;
 
 public abstract class RestService extends BaseScenario {
@@ -34,39 +32,46 @@ public abstract class RestService extends BaseScenario {
         return client.execute();
     }
 
-    public String executeAndMatch(String expected, MatchCondition... matchConditions) {
+    public HttpResponseWrapper executeAndMatch(String expected, MatchCondition... matchConditions) {
         return executeAndMatch(expected, null, matchConditions);
     }
 
-    public String executeAndMatch(String expected, Integer pollingTimeoutSeconds, MatchCondition... matchConditions) {
+    public HttpResponseWrapper executeAndMatch(String expected, Integer pollingTimeoutSeconds, MatchCondition... matchConditions) {
         return executeAndMatch(expected, pollingTimeoutSeconds, 3000, matchConditions);
     }
 
-    public String executeAndMatch(String expected, Integer pollingTimeoutSeconds,
-                                  long retryIntervalMillis, MatchCondition... matchConditions) {
+    public HttpResponseWrapper executeAndMatch(String expected, Integer pollingTimeoutSeconds,
+                                               long retryIntervalMillis, MatchCondition... matchConditions) {
         return executeAndMatch(expected, pollingTimeoutSeconds, retryIntervalMillis, null, matchConditions);
     }
 
-    public String executeAndMatch(String expected, Integer pollingTimeoutSeconds,
-                                  Long retryIntervalMillis, Double exponentialBackOff, MatchCondition... matchConditions) {
+    public HttpResponseWrapper executeAndMatch(String expected, Integer pollingDurationSeconds, long retryIntervalMillis, Double exponentialBackOff, MatchCondition... matchConditions) {
         logRequest(client);
         final HttpResponseReference responseRef = new HttpResponseReference();
-        String responseBody;
+        HttpResponseWrapper responseWrapper = null;
         try {
-            if (pollingTimeoutSeconds == null || pollingTimeoutSeconds == 0) {
+            if (pollingDurationSeconds == null || pollingDurationSeconds == 0) {
                 responseRef.set(client.execute());
                 scenarioProps.putAll(ObjectMatcher.matchHttpResponse(null, expected, responseRef.get(), matchConditions));
             } else {
                 scenarioProps.putAll(ObjectMatcher.matchHttpResponse(null, expected, () -> {
                     responseRef.set(client.execute());
                     return responseRef.get();
-                }, pollingTimeoutSeconds, retryIntervalMillis, exponentialBackOff, matchConditions));
+                }, pollingDurationSeconds, retryIntervalMillis, exponentialBackOff, matchConditions));
             }
         } finally {
             scenarioUtils.log("----------- EXPECTED RESPONSE -----------\n{}\n\n", expected);
-            responseBody = logAndGetResponse(responseRef.get());
+            try {
+                if (responseRef.get() != null) {
+                    responseWrapper = new HttpResponseWrapper(responseRef.get());
+                    logResponse(responseWrapper);
+                    responseRef.get().close();
+                }
+            } catch (Exception e) {
+                LOG.error(e);
+            }
         }
-        return responseBody;
+        return responseWrapper;
     }
 
     private void logRequest(HttpClient client) {
@@ -78,40 +83,16 @@ public abstract class RestService extends BaseScenario {
             scenarioUtils.log("Error logging request:\n{}", e);
             LOG.error(e);
         }
+        if (client.getProxyHost() != null) {
+            scenarioUtils.log("via PROXY HOST: {}", client.getProxyHost());
+        }
     }
 
-    private String logAndGetResponse(CloseableHttpResponse actual) {
-        HttpEntity entity = null;
-        String responseBody = null;
-        try {
-            if (actual != null) {
-                entity = actual.getEntity();
-                responseBody = (entity != null) ? EntityUtils.toString(entity) : null;
-                scenarioUtils.log("------------ ACTUAL RESPONSE ------------\nSTATUS: {} {}\nBODY: \n{}\nHEADERS:\n{}\n",
-                        actual.getStatusLine().getStatusCode(), actual.getStatusLine().getReasonPhrase(),
-                        (responseBody != null) ? JsonUtils.prettyPrint(responseBody) : "Empty data <∅>",
-                        Arrays.asList(actual.getAllHeaders()).toString());
-            }
-        } catch (IOException e) {
-            scenarioUtils.log("Error logging response:\n{}", e);
-            LOG.error(e);
-        } finally {
-            if (entity != null) {
-                try {
-                    EntityUtils.consume(entity);
-                } catch (IOException e) {
-                    LOG.error(e);
-                }
-            }
-            try {
-                if (actual != null) {
-                    actual.close();
-                }
-            } catch (IOException e) {
-                LOG.error(e);
-            }
-        }
-        return responseBody;
+    private void logResponse(HttpResponseWrapper response) {
+        scenarioUtils.log("------------ ACTUAL RESPONSE ------------\nSTATUS: {} {}\nBODY: \n{}\nHEADERS:\n{}\n",
+                response.getStatus(), response.getReasonPhrase(),
+                (response.getEntity() != null) ? JsonUtils.prettyPrint(response.getEntity().toString()) : "Empty data <∅>",
+                response.getHeaders());
     }
 
     private static class HttpResponseReference {
