750082161adbf23e38a3badb3ea2e9400bda9612
Florin Slevoaca <florin.slevoaca@ionos.com>
2021-10-11 15:55:09 +0300

    fix

--- src/test/java/com/cucumber/tutorial/listener/TestProgressListener.java
+++ src/test/java/com/cucumber/tutorial/listener/TestProgressListener.java
@@ -1,23 +1,42 @@
 package com.cucumber.tutorial.listener;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TestProgressListener implements TestExecutionListener {
+    private static final Logger LOG = LogManager.getLogger(TestProgressListener.class);
+    private final AtomicInteger testPassedCount = new AtomicInteger();
+    private final AtomicInteger testFailedCount = new AtomicInteger();
+    private final AtomicInteger testAbortedCount = new AtomicInteger();
+
+
     @Override
     public void testPlanExecutionStarted(TestPlan testPlan) {
-        System.out.println(testPlan.getConfigurationParameters());
-        System.out.println(testPlan.countTestIdentifiers(ti -> ti.isTest()));
+        LOG.info("TEST PLAN STARTED. Found {} tests", testPlan.countTestIdentifiers(TestIdentifier::isTest));
     }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
         if (testIdentifier.isTest()) {
-            System.out.println(testIdentifier.getDisplayName());
-            System.out.println(testIdentifier.getTags());
-            System.out.println(testExecutionResult.getStatus());
+            LOG.log(testExecutionResult.getStatus().equals(TestExecutionResult.Status.FAILED) ? Level.ERROR : Level.INFO,
+                    "{} | {}", testExecutionResult.getStatus(), testIdentifier.getDisplayName());
+            if (testExecutionResult.getStatus().equals(TestExecutionResult.Status.SUCCESSFUL)) {
+                testPassedCount.incrementAndGet();
+            } else if (testExecutionResult.getStatus().equals(TestExecutionResult.Status.FAILED)) {
+                testFailedCount.incrementAndGet();
+            } else if (testExecutionResult.getStatus().equals(TestExecutionResult.Status.ABORTED)) {
+                testAbortedCount.incrementAndGet();
+            }
+            LOG.log(testFailedCount.get() > 0 ? Level.WARN : Level.INFO,
+                    "Executed: {} | Passed: {} | Failed: {} | Aborted: {}", testPassedCount.get() + testFailedCount.get(),
+                    testPassedCount.get(), testFailedCount.get(), testAbortedCount.get());
         }
     }
 }
