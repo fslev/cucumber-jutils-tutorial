11a49de88e2a627d6a9ff9df2cbe8d7e61d3221b
florin <slevoaca.florin@gmail.com>
2022-10-16 16:00:25 +0300

    update

--- src/main/java/com/cucumber/tutorial/context/services/api/HttpService.java
+++ src/main/java/com/cucumber/tutorial/context/services/api/HttpService.java
@@ -3,10 +3,10 @@ package com.cucumber.tutorial.context.services.api;
 import com.cucumber.tutorial.context.BaseScenario;
 import com.cucumber.tutorial.util.DateUtils;
 import io.json.compare.util.JsonUtils;
+import io.jtest.utils.common.StringFormat;
 import io.jtest.utils.matcher.ObjectMatcher;
 import io.jtest.utils.matcher.condition.MatchCondition;
 import io.jtest.utils.matcher.http.PlainHttpResponse;
-import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
@@ -51,26 +51,6 @@ public abstract class HttpService extends BaseScenario {
         return Map.of("Content-Type", "application/json", "Accept", "application/json");
     }
 
-    protected static URI uri(String uri, Map<String, String> queryParams) {
-        URIBuilder uriBuilder;
-        try {
-            uriBuilder = new URIBuilder(uri);
-            if (queryParams != null) {
-                uriBuilder.addParameters(queryParams.entrySet().stream().map(param ->
-                        new BasicNameValuePair(param.getKey(), param.getValue())).collect(Collectors.toList()));
-            }
-            return uriBuilder.build();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected static Header[] headers(Map<String, String> headers) {
-        return headers.entrySet().stream()
-                .map(h -> new BasicHeader(h.getKey(), h.getValue())).collect(Collectors.toList())
-                .toArray(Header[]::new);
-    }
-
     public CloseableHttpResponse execute() {
         try {
             return client.execute(request);
@@ -107,7 +87,7 @@ public abstract class HttpService extends BaseScenario {
 
     public PlainHttpResponse executeAndMatch(String expected, Consumer<CloseableHttpResponse> consumer, Integer pollingDurationSeconds, long retryIntervalMillis,
                                              Double exponentialBackOff, MatchCondition... matchConditions) {
-        logRequest(client);
+        logRequest();
         logExpected(expected);
         final HttpResponseReference responseRef = new HttpResponseReference();
         PlainHttpResponse plainResponse = null;
@@ -131,7 +111,7 @@ public abstract class HttpService extends BaseScenario {
             if (responseRef.get() != null) {
                 try {
                     plainResponse = from(responseRef.get());
-                    logActual(plainResponse);
+                    logResponse(plainResponse);
                     if (consumer != null) {
                         consumer.accept(responseRef.get());
                     } else {
@@ -147,7 +127,7 @@ public abstract class HttpService extends BaseScenario {
         return plainResponse;
     }
 
-    private void logRequest(HttpClient client) {
+    private void logRequest() {
         try {
             scenarioUtils.log("------- API REQUEST ({}) -------\n{}\nHEADERS: {}\nBODY: {}\n\n",
                     DateUtils.currentDateTime(), request.getMethod() + " " +
@@ -165,12 +145,41 @@ public abstract class HttpService extends BaseScenario {
         scenarioUtils.log("----------------- EXPECTED RESPONSE -----------------\n{}\n\n", expected);
     }
 
-    private void logActual(PlainHttpResponse response) {
+    private void logResponse(PlainHttpResponse response) {
         scenarioUtils.log("------------------ ACTUAL RESPONSE ------------------\nSTATUS: {} {}\nBODY: \n{}\nHEADERS:\n{}\n",
                 response.getStatus(), response.getReasonPhrase(),
                 (response.getEntity() != null) ? prettyPrint(response.getEntity().toString()) : "Empty data <∅>",
                 response.getHeaders());
     }
+
+    protected static URI uri(String address, String path) {
+        return uri(address, path, null, null);
+    }
+
+    protected static URI uri(String address, String path, Map<String, String> pathParams) {
+        return uri(address, path, pathParams, null);
+    }
+
+    protected static URI uri(String address, String path, Map<String, String> pathParams, Map<String, String> queryParams) {
+        URIBuilder uriBuilder;
+        try {
+            uriBuilder = new URIBuilder(address + (path != null ? StringFormat.replaceProps(path, pathParams) : ""));
+            if (queryParams != null) {
+                uriBuilder.addParameters(queryParams.entrySet().stream().map(param ->
+                        new BasicNameValuePair(param.getKey(), param.getValue())).collect(Collectors.toList()));
+            }
+            return uriBuilder.build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected static Header[] headers(Map<String, String> headers) {
+        return headers.entrySet().stream()
+                .map(h -> new BasicHeader(h.getKey(), h.getValue())).collect(Collectors.toList())
+                .toArray(Header[]::new);
+    }
+
 
     private static String prettyPrint(String content) {
         try {
--- src/main/java/com/cucumber/tutorial/context/services/api/notebook/NotebookService.java
+++ src/main/java/com/cucumber/tutorial/context/services/api/notebook/NotebookService.java
@@ -2,7 +2,6 @@ package com.cucumber.tutorial.context.services.api.notebook;
 
 import com.cucumber.tutorial.context.services.api.HttpService;
 import io.cucumber.guice.ScenarioScoped;
-import io.jtest.utils.common.StringFormat;
 import org.apache.hc.core5.http.Method;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
@@ -15,32 +14,29 @@ public class NotebookService extends HttpService {
     public static final String NOTEBOOK_PATH = "/api/notebooks/#[notebookId]";
 
     public HttpService buildGetNotebooks(Map<String, String> queryParams) {
-        request = getDefaultRequest(Method.GET, uri(address() + NOTEBOOKS_PATH, queryParams));
+        request = getDefaultRequest(Method.GET, uri(address(), NOTEBOOKS_PATH, null, queryParams));
         return this;
     }
 
     public HttpService buildGetNotebook(String id) {
-        request = getDefaultRequest(Method.GET, uri(address() + StringFormat.replaceProps(NOTEBOOK_PATH,
-                Map.of("notebookId", id)), null));
+        request = getDefaultRequest(Method.GET, uri(address(), NOTEBOOK_PATH, Map.of("notebookId", id)));
         return this;
     }
 
     public HttpService buildCreateNotebook(String requestBody) {
-        request = getDefaultRequest(Method.POST, uri(address() + NOTEBOOKS_PATH, null));
+        request = getDefaultRequest(Method.POST, uri(address(), NOTEBOOKS_PATH));
         request.setEntity(new StringEntity(requestBody));
         return this;
     }
 
     public HttpService buildUpdateNotebook(String id, String requestBody) {
-        request = getDefaultRequest(Method.PATCH, uri(address() + StringFormat.replaceProps(NOTEBOOK_PATH,
-                Map.of("notebookId", id)), null));
+        request = getDefaultRequest(Method.PATCH, uri(address(), NOTEBOOK_PATH, Map.of("notebookId", id)));
         request.setEntity(new StringEntity(requestBody));
         return this;
     }
 
     public HttpService buildDeleteNotebook(String id) {
-        request = getDefaultRequest(Method.DELETE, uri(address() + StringFormat.replaceProps(NOTEBOOK_PATH,
-                Map.of("notebookId", id)), null));
+        request = getDefaultRequest(Method.DELETE, uri(address(), NOTEBOOK_PATH, Map.of("notebookId", id)));
         return this;
     }
 
