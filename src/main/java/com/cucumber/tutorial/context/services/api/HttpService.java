4a0c4dfd4389d504c52163d21320b0c54b6630f6
florin <slevoaca.florin@gmail.com>
2022-10-18 18:11:51 +0300

    enhance

--- src/main/java/com/cucumber/tutorial/context/services/api/HttpService.java
+++ src/main/java/com/cucumber/tutorial/context/services/api/HttpService.java
@@ -11,18 +11,22 @@ import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.core5.http.Method;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.apache.hc.core5.net.URIBuilder;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -32,10 +36,20 @@ import static com.cucumber.tutorial.util.PlainHttpResponseUtils.from;
 
 public abstract class HttpService extends BaseScenario {
 
-    private final static CloseableHttpClient client = HttpClients.custom()
-            .setConnectionManager(new PoolingHttpClientConnectionManager()).build();
+    private final static CloseableHttpClient client = client();
 
     protected HttpUriRequestBase request;
+
+    private static CloseableHttpClient client() {
+        try {
+            return HttpClients.custom()
+                    .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
+                            .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLContext.getInstance("TLS"),
+                                    new NoopHostnameVerifier())).build()).build();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     protected abstract String address();
 
