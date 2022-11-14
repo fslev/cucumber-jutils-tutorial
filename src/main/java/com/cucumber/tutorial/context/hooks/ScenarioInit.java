6a6dc42cbaa885f20618d406e920f1464b3d2ff6
florin <slevoaca.florin@gmail.com>
2022-11-14 14:55:42 +0200

    use assumption

--- src/main/java/com/cucumber/tutorial/context/hooks/ScenarioInit.java
+++ src/main/java/com/cucumber/tutorial/context/hooks/ScenarioInit.java
@@ -4,19 +4,20 @@ import com.cucumber.tutorial.config.Config;
 import com.cucumber.tutorial.context.BaseScenario;
 import io.cucumber.guice.ScenarioScoped;
 import io.cucumber.java.Before;
-import io.cucumber.java.PendingException;
 
 import java.util.Map;
 
 import static com.cucumber.tutorial.config.Config.PROPS;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @ScenarioScoped
 public class ScenarioInit extends BaseScenario {
 
     @Before(order = 0)
     public void safetyCheck() {
-        if (Config.isProdEnv() && !scenarioUtils.getScenario().getSourceTagNames().contains("@prod")) {
-            throw new PendingException("Safety exception: I am not running scenarios on LIVE environment without '@prod' tag");
+        if (Config.isProdEnv()) {
+            assumeTrue(scenarioUtils.getScenario().getSourceTagNames().contains("@prod"),
+                    "LIVE safety: I am not running scenarios on LIVE environment without '@prod' tag");
         }
     }
 
