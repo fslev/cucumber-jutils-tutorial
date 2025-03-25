2d60a0fdf7d4df57fc09a2894f8df598b1df0ef7
fslevoaca <florin.slevoaca@ionos.com>
2025-03-25 17:22:22 +0200

    enhance JSON processing error message

--- src/main/java/com/cucumber/tutorial/util/PlainHttpResponseUtils.java
+++ src/main/java/com/cucumber/tutorial/util/PlainHttpResponseUtils.java
@@ -32,11 +32,12 @@ public class PlainHttpResponseUtils {
             return mapper.readValue(content, PlainHttpResponse.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Cannot convert to PlainHttpResponse object:\n" + content+"\n\nExpected:\n{\n" +
-                    "  [optional] \"status\": <object>,\n" +
-                    "  [optional] \"reason\": \"<text>\",\n" +
-                    "  [optional] \"body\": <object>,\n" +
-                    "  [optional] \"headers\": <json array> ex: [ {\"h1\": \"v1\"}, {\"h2\": \"v2\"} ]\n" +
-                    "}", e);        }
+                    "  [optional] \"status\": <JsonNode>,\n" +
+                    "  [optional] \"reason\": \"<TextNode>\",\n" +
+                    "  [optional] \"body\": <JsonNode>,\n" +
+                    "  [optional] \"headers\": <ArrayNode> ex: [ {\"h1\": \"v1\"}, {\"h2\": \"v2\"} ]\n" +
+                    "}", e);
+        }
     }
 
     public static PlainHttpResponse from(ClassicHttpResponse response) {
